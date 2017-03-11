c21aa1bbb4a561a0d9e7a53d42e1fbaca5f06cea
yuanmingfang <xinghuoyuanfang@126.com>
2017-03-11 15:32:55 +0800

    version 2.0

--- test/src/main/Test.java
+++ test/src/main/Test.java
@@ -5,6 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world! ");
+		
+		System.out.println("test git success ! ");
+		
+		
+		
 	}
 
 }
